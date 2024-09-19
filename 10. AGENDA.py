e9b0fa819f4f8305468372c674a991fbbc55268b
𝙋𝙚𝙙𝙧𝙤 𝙀𝙙𝙪𝙖𝙧𝙙𝙤 <159453815+PedroLagoX@users.noreply.github.com>
2024-09-19 14:31:36 +0000

    Agenda

---
+++ 10. AGENDA.py
@@ -0,0 +1,16 @@
+import os
+os.system("cls || clear")
+print("AGENDA - PEDRO EDUARDO")
+
+print("Segunda-Feira")
+print("07:30 às 11:30 = SENAI")
+print("11:30 às 12:00 = BANHO")
+print("12:00 às 13:00 = ALMOÇO")
+print("13:00 às 15:00 = SONECA")
+print("15:00 às 18:00 = ESTUDO")
+print("19:00 às 22:00 = LAZER")
+print("22:00 às 06:00 = DORMIR")
+print("06:00 às 07:00 = SE ARRUMAR")
+print("07:00 às 07:30 = IR PRO SENAI")
+
+
